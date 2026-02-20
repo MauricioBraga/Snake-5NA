9dccbc302572d58a2f2366176d32e130a2a24cf4
Maurício Braga <mb@gmail.com>
2026-02-19 21:31:09 -0300

    Adicionado chaveamento de estados ao pressionar a barra de espaço.

--- Assets/scripts/AberturaGameState.cs
+++ Assets/scripts/AberturaGameState.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class AberturaGameState: GameBaseState
 {
     public override void enterState(GameStateManager gameState)  {
-        Debug.Log("Entramos na tela inicial");
+        Debug.Log("Entramos na tela inicial");      
     }
     public override void updateState(GameStateManager gameState)  {
-
+        if (Input.GetKeyDown(KeyCode.Space))  {
+            gameState.switchState(gameState.telaCreditosState);
+        }
     }
     public override void leaveState(GameStateManager gameState)  {
         Debug.Log("Saindo da tela inicial");
--- Assets/scripts/CreditosGameState.cs
+++ Assets/scripts/CreditosGameState.cs
@@ -4,9 +4,13 @@ public class CreditosGameState: GameBaseState
 {
     public override void enterState(GameStateManager gameState)  {
         Debug.Log("Entramos na tela de créditos");
+
+        
     }
     public override void updateState(GameStateManager gameState)  {
-
+        if (Input.GetKeyDown(KeyCode.Space))  {
+            gameState.switchState(gameState.telaInicialState);
+        }
     }
     public override void leaveState(GameStateManager gameState)  {
         Debug.Log("Saindo da tela de créditos");
