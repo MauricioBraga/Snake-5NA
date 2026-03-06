73b245f758d862f601a1a01776db97ab37f4c523
Maurício Braga <mb@gmail.com>
2026-03-05 21:28:12 -0300

    Adicionada a temporização dos estados abertura e créditos.

--- Assets/scripts/AberturaGameState.cs
+++ Assets/scripts/AberturaGameState.cs
@@ -3,12 +3,28 @@ using UnityEngine;
 public class AberturaGameState: GameBaseState
 {
     private GameObject telaInicialJogo;
+    private float tempo_mudanca = 0.5f;
+    private float timer;
+    private int contador;
+
     public override void enterState(GameStateManager gameState)  {
         Debug.Log("Entramos na tela inicial");
         telaInicialJogo = GameObject.Find("tela_nova_inicial_Snake_1280_1060_0");
         telaInicialJogo.GetComponent<SpriteRenderer>().enabled = true;      
+        timer = 0;
+        contador = 15;
     }
     public override void updateState(GameStateManager gameState)  {
+        if (timer < tempo_mudanca)  {
+            timer = timer + Time.deltaTime;
+        }
+        else  {
+            contador--;
+            timer = 0;
+            if (contador < 0)  {
+                gameState.switchState(gameState.telaCreditosState);    
+            }
+        }
         if (Input.GetKeyDown(KeyCode.Space))  {
             gameState.switchState(gameState.jogoState);
         }
--- Assets/scripts/CreditosGameState.cs
+++ Assets/scripts/CreditosGameState.cs
@@ -3,14 +3,28 @@ using UnityEngine;
 public class CreditosGameState: GameBaseState
 {
      private GameObject telaCreditosJogo;
+      private float tempo_mudanca = 0.5f;
+    private float timer;
+    private int contador;
     public override void enterState(GameStateManager gameState)  {
         Debug.Log("Entramos na tela de créditos");
         telaCreditosJogo = GameObject.Find("tela_creditos_Snake_1280_1060_0");
         telaCreditosJogo.GetComponent<SpriteRenderer>().enabled = true;      
-
+        timer = 0;
+        contador = 10;
         
     }
     public override void updateState(GameStateManager gameState)  {
+        if (timer < tempo_mudanca)  {
+            timer = timer + Time.deltaTime;
+        }
+        else  {
+            contador--;
+            timer = 0;
+            if (contador < 0)  {
+                gameState.switchState(gameState.telaInicialState);    
+            }
+        }
         if (Input.GetKeyDown(KeyCode.Space))  {
             gameState.switchState(gameState.telaInicialState);
         }
